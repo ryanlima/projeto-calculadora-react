37dd11df06814a6bce8a7debb980c85103a88180
Ryan Lima <ryanlima08021997@gmail.com>
2019-07-03 15:24:32 -0300

    Updates

---
+++ precache-manifest.03dde3239499fc9052517d6f83d0afbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8142c619b81f338f9f33f71ee1a1da5a",
+    "url": "/projeto-calculadora-react/index.html"
+  },
+  {
+    "revision": "e231c37b1dc5b74a369d",
+    "url": "/projeto-calculadora-react/static/css/main.4b8d1123.chunk.css"
+  },
+  {
+    "revision": "32bfe6000434f04292fa",
+    "url": "/projeto-calculadora-react/static/js/2.b3b50664.chunk.js"
+  },
+  {
+    "revision": "e231c37b1dc5b74a369d",
+    "url": "/projeto-calculadora-react/static/js/main.6164ff7e.chunk.js"
+  },
+  {
+    "revision": "44af691b8f88b4a949dc",
+    "url": "/projeto-calculadora-react/static/js/runtime~main.229fbea1.js"
+  },
+  {
+    "revision": "d1e42f1b6d0c32271f8e68515563a213",
+    "url": "/projeto-calculadora-react/static/media/Roboto-Thin.d1e42f1b.ttf"
+  }
+]);
