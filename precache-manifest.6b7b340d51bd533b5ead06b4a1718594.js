1c9b7ee494e72858632892f9304cc1d6198d0c65
Ryan Lima <ryanlima08021997@gmail.com>
2019-07-03 15:07:30 -0300

    Updates

---
+++ precache-manifest.6b7b340d51bd533b5ead06b4a1718594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c691c15dd0c4b98b18c2020ee8599e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee2346df2f97ef18127",
+    "url": "/static/css/main.c1c2c1ec.chunk.css"
+  },
+  {
+    "revision": "32bfe6000434f04292fa",
+    "url": "/static/js/2.b3b50664.chunk.js"
+  },
+  {
+    "revision": "dee2346df2f97ef18127",
+    "url": "/static/js/main.6e4a2168.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1e42f1b6d0c32271f8e68515563a213",
+    "url": "/static/media/Roboto-Thin.d1e42f1b.ttf"
+  }
+]);
